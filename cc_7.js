f1f0ae1418fbb54f719f736df4e04a71bb1217c8
mallikanicole <malmichael23@gmail.com>
2025-02-13 15:33:40 -0500

    added .toFixed

--- cc_7.js
+++ cc_7.js
@@ -4,8 +4,19 @@ function calculateInvoice(subtotal,taxRate,discount){
     return (subtotal+(subtotal*taxRate))-discount
 };//create function to calculate invoice
 
-console.log (`Total Invoice:$${calculateInvoice(100, 0.08, 5)}`); // Expected output: "Total Invoice: $103.00"
-console.log(`Total Invoice: $${calculateInvoice(500, 0.1, 20)}`); // Expected output: "Total Invoice: $530.00"
-//!!!!!!!!!!NEED TO FIGURE OUT HOW TO ADD .00 TO PRICE
+console.log (`Total Invoice:$${calculateInvoice(100, 0.08, 5).toFixed(2)}`); // Expected output: "Total Invoice: $103.00"
+console.log(`Total Invoice: $${calculateInvoice(500, 0.1, 20).toFixed(2)}`); // Expected output: "Total Invoice: $530.00"
 
-//Task 2
+
+//Task 2 Function Expression
+
+function calculateHourlyWage(salary, hoursPerWeek){
+    return salary/(hoursPerWeek*52)
+};//create function to calculate hourly wage
+
+console.log(`Hourly Wage:$${calculateHourlyWage(52000, 40).toFixed(2)}`); // Expected output: "Hourly Wage: $25.00"
+console.log(`Hourly Wage:$${calculateHourlyWage(75000, 35).toFixed(2)}`); // Expected output: "Hourly Wage: $41.21"
+
+//Task 3 Arrow Function
+
+
