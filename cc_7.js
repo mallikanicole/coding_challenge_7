026858d5d4c8026f63a347614e698a3d00193cff
mallikanicole <malmichael23@gmail.com>
2025-02-13 11:45:53 -0500

    Task 1 - Customer Invoice Calculation

--- cc_7.js
+++ cc_7.js
@@ -1,1 +1,11 @@
-//Task 1 
+//Task 1 Function Declaration
+
+function calculateInvoice(subtotal,taxRate,discount){
+    return (subtotal+(subtotal*taxRate))-discount
+};//create function to calculate invoice
+
+console.log (`Total Invoice:$${calculateInvoice(100, 0.08, 5)}`); // Expected output: "Total Invoice: $103.00"
+console.log(`Total Invoice: $${calculateInvoice(500, 0.1, 20)}`); // Expected output: "Total Invoice: $530.00"
+//!!!!!!!!!!NEED TO FIGURE OUT HOW TO ADD .00 TO PRICE
+
+//Task 2
